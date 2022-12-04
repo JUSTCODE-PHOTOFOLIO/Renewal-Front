c3103ede8e33e21dae7ce196cf51f81edf4f5058
DawonOh <89020079+DawonOh@users.noreply.github.com>
2022-12-04 21:10:55 +0900

    Revert "게시글, 댓글, 댓글 등록시 현재 댓글 input value 삭제 기능 추가"

--- src/components/CardDetailContent/Reply/Reply.js
+++ src/components/CardDetailContent/Reply/Reply.js
@@ -1,71 +1,13 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import './Reply.scss';
 
-const Reply = ({
-  kor_name,
-  comment,
-  created_at,
-  user_id,
-  id,
-  URI,
-  posting_id,
-}) => {
-  const idCheck = localStorage.getItem('id');
-  const [menuBtn, setMenuBtn] = useState(false); //메뉴버튼 클릭 상태
-
-  //메뉴버튼 클릭시 버튼의 상태 변경
-  const menuClick = () => {
-    if (menuBtn == false) {
-      setMenuBtn(true);
-    } else {
-      setMenuBtn(false);
-    }
-  };
-
-  const [deletedReply, setDeletedReply] = useState(''); //삭제할 댓글
-
-  //댓글 삭제 fetch
-  const deleteReply = () => {
-    alert('댓글을 삭제하시겠습니까?');
-    fetch('http://' + URI + ':8000/comments', {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('token'),
-      },
-      body: JSON.stringify({ comment_id: id, posting_id: posting_id }),
-    })
-      .then(res => res.json())
-      .then(res => setDeletedReply(res.deleted_comment_id));
-  };
-
+const Reply = ({ kor_name, comment, created_at }) => {
   return (
     <>
-      {deletedReply !== id && (
-        <div className="detail-reply-card">
-          <div className="reply-top-wrapper">
-            <div>
-              <div className="detail-reply-writer">{kor_name}</div>
-            </div>
-            {idCheck == user_id && (
-              <span className="menu" onClick={menuClick} />
-            )}
-            {menuBtn && (
-              <>
-                <div className="selectMenuWrapper">
-                  <div className="selectMenu">수정</div>
-                  <div className="selectMenu" onClick={deleteReply}>
-                    삭제
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="detail-reply-content">{comment}</div>
-          <div className="detail-reply-date">{created_at}</div>
-        </div>
-      )}
+      <div className="detail-reply-card">
+        <div className="detail-reply-writer">{kor_name}</div>
+        <div className="detail-reply-content">{comment}</div>
+        <div className="detail-reply-date">{created_at}</div>
+      </div>
     </>
   );
 };
